a0017413a79df4b4a0e7e1a3f3f47c4bc42dc798
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-11-10 08:30:58 -0500

    pulled countries from api and started cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { MenuItem, FormControl, Select, InputLabel} from "@material-ui/core";
 import './App.css';
+import InfoBox from './InfoBox';
+
 
 function App() {
+  //! STATE
+  const [ countries, setCountries ] = useState([]);
+  const [ country, setCountry ] = useState('worldwide');
+
+
+  //? Disease Api
+//? https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    const  getCountriesData = async()=>{
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then(res => res.json())
+      .then((data)=>{
+        const countries = data.map((country)=>(
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }
+        ));
+        setCountries(countries);
+      });
+    };
+    getCountriesData();
+  }, [countries]);
+
+  const onCountryChange = (e)=>{
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+    {/* Header */}
+        <h1>COVID-19 Tracker</h1>
+        {/* title + drop down */}
+        <FormControl className="app__dropdown">
+          <InputLabel  id="thing" >Country</InputLabel>
+          <Select
+          labelId="thing"
+          variant="outlined"
+          value={country} onChange={}>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+        {/* Map through countries */}
+            {countries.map(country =>{
+              return <MenuItem value={country.value}>{country.name}</MenuItem>
+            })}
+          </Select>
+        </FormControl> 
+      </div>
+      <div className="app__stats">
+            <InfoBox title="Coronavirus Cases" />
+            
+            <InfoBox title="Recovered" />
+            
+            <InfoBox title="Deaths" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
